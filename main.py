1fb61ba5359e34049ab6095eb1143cde4898da71
waldembergdev <waldemberg.pereira@outlook.com>
2025-04-24 22:05:02 -0300

    feat: implementa interface principal com consulta do tempo via OpenWeather

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
-from pprint import pprint
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 # carregando o ambiente virtual
 load_dotenv(r'.venv/.env')
@@ -12,7 +12,7 @@ TOKEN = os.getenv('TOKEN')
 
 def obter_latitude_longitude(cidade: str):
     # obtendo a latitude e longitude
-    url_latitude_longitude = f'http://api.openweathermap.org/geo/1.0/direct'
+    url_latitude_longitude = 'http://api.openweathermap.org/geo/1.0/direct'
 
     # passando os parâmetros obrigatórios
     params = {
@@ -36,7 +36,7 @@ def obter_latitude_longitude(cidade: str):
 
 
 def obter_previsao_tempo(latitude, logintude):
-    url = f'https://api.openweathermap.org/data/2.5/weather'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
 
     params = {
         'lat': latitude,
@@ -54,13 +54,46 @@ def obter_previsao_tempo(latitude, logintude):
     except requests.HTTPError as e:
         print(f'Erro ao realizar a requisição: {e}')
 
+
 def main():
-    pass
+    st.header(body='Web App Tempo', divider=True)
+    st.write(
+        'Acesse a OpenWeather clicando '
+        '[aqui](https://openweathermap.org/current).'
+        )
+    local = st.text_input(label='Busque uma cidade: ')
+    if not local:
+        st.stop()
+    dados_latitude_longitude = obter_latitude_longitude(local)
+    if not dados_latitude_longitude:
+        st.warning(
+            f'A cidade {local} não foi encontrada!'
+            'Procure por outra cidade.'
+            )
+    latitude = dados_latitude_longitude[0]
+    Longitude = dados_latitude_longitude[1]
+    dados_tempo = obter_previsao_tempo(latitude=latitude, logintude=Longitude)
+    # salvando os dados em uma váriavel
+    tempo_atual = dados_tempo['weather'][0].get('description')
+    temperatura_atual = dados_tempo['main'].get('temp')
+    temperatura_maxima = dados_tempo['main'].get('temp_max')
+    temperatura_minima = dados_tempo['main'].get('temp_min')
+    sensacao_termica = dados_tempo['main'].get('feels_like')
+    umidade = dados_tempo['main'].get('humidity')
+    cortura_nuvens = dados_tempo['clouds'].get('all')
+    # criando uma visualização no streamlit
+    st.metric(label='Tempo atual', value=tempo_atual)
+    # dividindo a aplicação em duas colunas
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(label='Temperatura atual (ºC)', value=temperatura_atual)
+        st.metric(label='Temperatura máxima (ºC)', value=temperatura_maxima)
+        st.metric(label='Temperatura mínima (ºC)', value=temperatura_minima)
+    with col2:
+        st.metric(label='Sensação térmica (ºC)', value=sensacao_termica)
+        st.metric(label='Humidade (%)', value=umidade)
+        st.metric(label='Cobertura de nuvens (%)', value=cortura_nuvens)
 
 
 if __name__ == '__main__':
-    resultado = obter_latitude_longitude('Duque de caxias')
-
-    previsao = obter_previsao_tempo(resultado[0], resultado[1])
-
-    pprint(previsao)
+    main()
